6c2a4f8493a465f3d8cb19df799862cdd6582ac1
Reus134 <839745095@qq.com>
2024-11-03 02:11:17 -0800

    upadte in 20241103

---
+++ thread/alipay.c
@@ -0,0 +1,16 @@
+#include "thread.h"
+
+unsigned int balance = 100;
+
+int Alipay_withdraw(int amt) {
+    if (balance >= amt) {
+        balance -= amt;
+        return SUCCESS;
+    } else {
+        return FAIL;
+    }
+}
+int main () {
+
+
+}
