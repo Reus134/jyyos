e47983c9b98bbcf79e45c95737b6aeefab34fa79
tracer-nju <tracer@nju.edu.cn>
2024-11-24 07:51:07 -0800

    Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  07:51:07 up  8:59,  0 users,  load average: 0.04, 0.03, 0.00

--- thread/lab/libco/co.c
+++ thread/lab/libco/co.c
@@ -1,147 +1,201 @@
 #include "co.h"
-#include <stdlib.h>
+
+#include <assert.h>
 #include <setjmp.h>
-#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-#define STACK_SIZE 64
-#define CO_MAX_NUM 32
-typedef unsigned char uint8_t;
-
-struct co *current = NULL;
-struct co *active_cos[10] = {0};
-int active_co_numbers = 0;
-int current_index = 0;
+struct co *current;
 
 enum co_status {
-    CO_NEW = 1, // 新创建，还未执行过
-    CO_RUNNING, // 已经执行过
-    CO_WAITING, // 在 co_wait 上等待
-    CO_DEAD,    // 已经结束，但还未释放资源
+  CO_NEW = 1,  // 新创建，还未执行过
+  CO_RUNNING,  // 已经执行过
+  CO_WAITING,  // 在 co_wait 上等待
+  CO_DEAD,     // 已经结束，但还未释放资源
 };
+
+#define K 1024
+#define STACK_SIZE (64 * K)
 
 struct co {
   const char *name;
-  void (*func)(void *); // co_start 指定的入口地址和参数
+  void (*func)(void *);  // co_start 指定的入口地址和参数
   void *arg;
 
-  enum co_status status;  // 协程的状态
-  struct co*     waiter;  // 是否有其他协程在等待当前协程
-  jmp_buf        context; // 寄存器现场 (setjmp.h)
-  uint8_t        stack[STACK_SIZE]; // 协程的栈
+  enum co_status status;            // 协程的状态
+  struct co *waiter;                // 是否有其他协程在等待当前协程
+  jmp_buf context;                  // 寄存器现场 (setjmp.h)
+  unsigned char stack[STACK_SIZE];  // 协程的堆栈
 };
 
+typedef struct CONODE {
+  struct co *coroutine;
+
+  struct CONODE *fd, *bk;
+} CoNode;
+
+static CoNode *co_node = NULL;
+/*
+ * 如果co_node == NULL，则创建一个新的双向循环链表即可，并返回
+ * 如果co_node != NULL, 则在co_node和co_node->fd之间插入，仍然返回co_node的值
+ */
+static void co_node_insert(struct co *coroutine) {
+  CoNode *victim = (CoNode *)malloc(sizeof(CoNode));
+  assert(victim);
+
+  victim->coroutine = coroutine;
+  if (co_node == NULL) {
+    victim->fd = victim->bk = victim;
+    co_node = victim;
+  } else {
+    victim->fd = co_node->fd;
+    victim->bk = co_node;
+    victim->fd->bk = victim->bk->fd = victim;
+  }
+}
+
+/*
+ * 如果当前只剩node一个，则返回该一个
+ * 否则，拉取当前co_node对应的协程，并沿着bk方向移动
+ */
+static CoNode *co_node_remove() {
+  CoNode *victim = NULL;
+
+  if (co_node == NULL) {
+    return NULL;
+  } else if (co_node->bk == co_node) {
+    victim = co_node;
+    co_node = NULL;
+  } else {
+    victim = co_node;
+
+    co_node = co_node->bk;
+    co_node->fd = victim->fd;
+    co_node->fd->bk = co_node;
+  }
+
+  return victim;
+}
+
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
+  struct co *coroutine = (struct co *)malloc(sizeof(struct co));
+  assert(coroutine);
 
-    if (active_co_numbers == 0) {
-        struct co *main_co = malloc(sizeof(struct co)); 
-        main_co->status = CO_NEW;
-        main_co->name = "main";
-        main_co->waiter = NULL;
-        active_cos[current_index] = main_co;
-    }
+  coroutine->name = name;
+  coroutine->func = func;
+  coroutine->arg = arg;
+  coroutine->status = CO_NEW;
+  coroutine->waiter = NULL;
 
-    struct co *init_co = malloc(sizeof(struct co)); 
-    init_co->status = CO_NEW;
-    init_co->name = name;
-    init_co->arg = arg;
-    init_co->func = func;
-    init_co->waiter = NULL;
-    //共享内存里的current 指针指向cur_co
-    //current = cur_co;
-    active_co_numbers++;
-    active_cos[active_co_numbers] = init_co;
-    // if(setjmp(init_co->context) == 0) {
-    //     printf("%s init setjmp\n",init_co->name);
-    // } else {
-    //     active_cos[current_index]->func(active_cos[current_index]->arg);
-    //     active_cos[current_index]->status = CO_DEAD;
-    //     active_co_numbers--;
-    //     if(!active_co_numbers) {
-    //         //回主协程（main）
-    //         longjmp(active_cos[0]->context, 1);
-    //     }
-    //     co_yield();
-    // }
+  co_node_insert(coroutine);
+  return coroutine;
+}
 
-    return init_co;
+void co_wait(struct co *coroutine) {
+  assert(coroutine);
+
+  if (coroutine->status != CO_DEAD) {
+    coroutine->waiter = current;
+    current->status = CO_WAITING;
+    co_yield ();
+  }
+
+  /*
+   * 释放coroutine对应的CoNode
+   */
+  while (co_node->coroutine != coroutine) {
+    co_node = co_node->bk;
+  }
+
+  assert(co_node->coroutine == coroutine);
+
+  free(coroutine);
+  free(co_node_remove());
 }
 
 /*
-1.此时协程已经结束 (func 返回).这是完全可能的。此时,co_wait 应该直接回收资源。
-2.此时协程尚未结束，因此 co_wait 不能继续执行，必须调用 co_yield 切换到其他协程执行，直到协程结束后唤醒
-*/
-// current需要等待co执行完 当前协程需要等待co执行
-void co_wait(struct co *co) {
-    while(co->status != CO_DEAD) {
-        co_yield();
-        //co->status = CO_DEAD;
-    }
-    // 走到这说明一家CO_DEAD
-    free(co);
-}
-
-
-/*
-1.为每一个协程分配独立的堆栈；堆栈顶的指针由 %rsp 寄存器确定；
-2.在 co_yield 发生时，将寄存器保存到属于该协程的 struct co 中 (包括 %rsp)；
-3.切换到另一个协程执行，找到系统中的另一个协程，然后恢复它 struct co 中的寄存器现场 (包括 %rsp)。
-*/
-/*co_yield 会将当前运行协程的寄存器保存到共享内存中，
-然后选择一个另一个协程，将寄存器加载到 CPU 上，
-就完成了 “状态机的切换”；*/
-/*
-    asm(
-        内嵌汇编指令
-        ：输出操作数
-        ：输入操作数
-        ：破坏描述
-    );
-*/
-
-static int get_random_except_current(int index, int bound) {
-    int num;
-    do {
-        num = rand() % bound + 1; // 生成 [1, bound] 范围的随机数
-    } while (num == index);        // 排除 index
-    return num;
-}
-
-static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
-  asm volatile (
+ * 切换栈，即让选中协程的所有堆栈信息在自己的堆栈中，而非调用者的堆栈。保存调用者需要保存的寄存器，并调用指定的函数
+ */
+static inline void stack_switch_call(void *sp, void *entry, void *arg) {
+  asm volatile(
 #if __x86_64__
-    "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
-      : : "b"((uintptr_t)sp), "d"(entry), "a"(arg) : "memory"
+      "movq %%rcx, 0(%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
+      :
+      : "b"((uintptr_t)sp - 16), "d"((uintptr_t)entry), "a"((uintptr_t)arg)
 #else
-    "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
-      : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg) : "memory"
+      "movl %%ecx, 4(%0); movl %0, %%esp; movl %2, 0(%0); call *%1"
+      :
+      : "b"((uintptr_t)sp - 8), "d"((uintptr_t)entry), "a"((uintptr_t)arg)
+#endif
+  );
+}
+/*
+ * 从调用的指定函数返回，并恢复相关的寄存器。此时协程执行结束，以后再也不会执行该协程的上下文。这里需要注意的是，其和上面并不是对称的，因为调用协程给了新创建的选中协程的堆栈，则选中协程以后就在自己的堆栈上执行，永远不会返回到调用协程的堆栈。
+ */
+static inline void restore_return() {
+  asm volatile(
+#if __x86_64__
+      "movq 0(%%rsp), %%rcx"
+      :
+      :
+#else
+      "movl 4(%%esp), %%ecx"
+      :
+      :
 #endif
   );
 }
 
-void co_yield() {
-    //保留当前的context
-    int ret = -1;
-    //ret = setjmp(active_cos[current_index]->context);
-    int old_index = current_index;
-    //选择的协程是新创建的，此时该协程还没有执行过任何代码，我们需要首先执行 stack_switch_call 切换栈，然后开始执行协程的代码；
-    //选择的协程是调用 yield() 切换出来的，此时该协程已经调用过 setjmp 保存寄存器现场，我们直接 longjmp 恢复寄存器现场即可。
-    current_index = get_random_except_current(current_index, active_co_numbers);
-    struct co *current_co = active_cos[current_index];
-    //struct co* current_co = active_cos[current_index];
-    ret = setjmp(active_cos[old_index]->context);
-    
-    active_cos[old_index]->status = CO_WAITING;
-    if (ret == 0) {
-        if (current_co->status == CO_NEW) {
-            current_co->status = CO_RUNNING;
-            stack_switch_call((uintptr_t)(current_co->stack + STACK_SIZE),(uintptr_t)(current_co->func), (uintptr_t)(current_co->arg));
-        } else {
-            active_cos[current_index]->status = CO_RUNNING;
-            longjmp(active_cos[current_index]->context, 1);
-        }
-    } else {
-        printf("co index = %d return to this\n",current_index);
-        active_cos[current_index]->status = CO_DEAD;
+#define __LONG_JUMP_STATUS (1)
+void co_yield () {
+  int status = setjmp(current->context);
+  if (!status) {
+    //此时开始查找待选中的进程，因为co_node应该指向的就是current对应的节点，因此首先向下移动一个，使当前线程优先级最低
+    co_node = co_node->bk;
+    while (!((current = co_node->coroutine)->status == CO_NEW || current->status == CO_RUNNING)) {
+      co_node = co_node->bk;
     }
+
+    assert(current);
+
+    if (current->status == CO_RUNNING) {
+      longjmp(current->context, __LONG_JUMP_STATUS);
+    } else {
+      ((struct co volatile *)current)->status = CO_RUNNING;  //这里如果直接赋值，编译器会和后面的覆写进行优化
+
+      // 栈由高地址向低地址生长
+      stack_switch_call(current->stack + STACK_SIZE, current->func, current->arg);
+      //恢复相关寄存器
+      restore_return();
+
+      //此时协程已经完成执行
+      current->status = CO_DEAD;
+
+      if (current->waiter) {
+        current->waiter->status = CO_RUNNING;
+      }
+      co_yield ();
+    }
+  }
+
+  assert(status && current->status == CO_RUNNING);  //此时一定是选中的进程通过longjmp跳转到的情况执行到这里
+}
+
+static __attribute__((constructor)) void co_constructor(void) {
+  current = co_start("main", NULL, NULL);
+  current->status = CO_RUNNING;
+}
+
+static __attribute__((destructor)) void co_destructor(void) {
+  if (co_node == NULL) {
+    return;
+  }
+
+  while (co_node) {
+    current = co_node->coroutine;
+    free(current);
+    free(co_node_remove());
+  }
 }
